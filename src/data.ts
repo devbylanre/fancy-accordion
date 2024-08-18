94969f700c2c301c2c6de994195107ae36192b9b
devbylanre <devbylanre@gmail.com>
2024-08-18 13:19:02 +0100

    added accordion data

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+const accordions = [
+  {
+    title: 'What is Streamline Pro?',
+    content:
+      'Streamline Pro is a cutting-edge project management tool designed to help teams organize tasks, streamline workflows, and boost productivity through intuitive features and real-time collaboration.',
+    icon: `<svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 256 256'
+      >
+        <path d='M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z'></path>
+      </svg>`,
+  },
+  {
+    title: 'How does Streamline Pro work?',
+    content:
+      'Streamline Pro works by allowing you to create and manage tasks, set deadlines, assign team members, and track progress all in one place. Simply sign up, create a project, and start adding tasks. Your team members can collaborate in real-time, making it easy to keep everyone on the same page.',
+  },
+  {
+    title: 'How do I get started with Streamline Pro?',
+    content:
+      "Getting started is easy! Just sign up with your email, choose a plan, and you can start creating your first project in minutes. Once you're in, you can invite team members, set up your tasks, and begin collaborating right away.",
+  },
+  {
+    title: 'Is there a free trial available?',
+    content:
+      'Yes, we offer a 14-day free trial so you can explore all the features of Streamline Pro before making a commitment. Sign up now to get started with your trial and see how Streamline Pro can transform your workflow.',
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M234.29,114.85l-45,38.83L203,211.75a16.4,16.4,0,0,1-24.5,17.82L128,198.49,77.47,229.57A16.4,16.4,0,0,1,53,211.75l13.76-58.07-45-38.83A16.46,16.46,0,0,1,31.08,86l59-4.76,22.76-55.08a16.36,16.36,0,0,1,30.27,0l22.75,55.08,59,4.76a16.46,16.46,0,0,1,9.37,28.86Z"></path></svg>`,
+  },
+  {
+    title: 'Can I upgrade or downgrade my plan later?',
+    content:
+      'Absolutely! You can upgrade or downgrade your plan at any time from your account settings. We offer flexible plans to suit teams of all sizes, and our customer support team is always here to help you make the switch.',
+  },
+];
+
+export default accordions;
